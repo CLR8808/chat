9f8348d2143941b145d50e21b08d3e71f61c24c7
CLR8808 <car89992@gmail.com>
2026-08-06 08:08:11 -0700

    Configuracion de produccion y soporte Netlify (SPA redirects, prod environment y presupuesto de estilos)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBTxcogz01HMruIuFwxGKxohrd7OZn0TrU",
+    authDomain: "chats-f56f1.firebaseapp.com",
+    projectId: "chats-f56f1",
+    storageBucket: "chats-f56f1.firebasestorage.app",
+    messagingSenderId: "37690167915",
+    appId: "1:37690167915:web:2d6cbcb39fbb2b5f19d72e"
+  }
 };
+
+export const app = initializeApp(environment.firebaseConfig);
+export const db = getFirestore(app);
